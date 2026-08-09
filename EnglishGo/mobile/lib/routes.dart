1b7adffadf2f5dbf94a6e54a9da228ab7701429a
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 15:54:45 +0100

    Add AppRoutes class with route constants

--- EnglishGo/mobile/lib/routes.dart
+++ EnglishGo/mobile/lib/routes.dart
@@ -1,1 +1,10 @@
-
+class AppRoutes {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String lessons = '/lessons';
+  static const String speaking = '/speaking';
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+  static const String premium = '/premium';
+}
