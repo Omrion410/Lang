7153c8e2ee5ca8f2bd787c5022310154282a737d
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 15:58:23 +0100

    Add UserModel, LessonModel, and AchievementModel classes

--- EnglishGo/mobile/lib/models.dart
+++ EnglishGo/mobile/lib/models.dart
@@ -1,1 +1,43 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String language;
 
+  const UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.language = 'en',
+  });
+}
+
+class LessonModel {
+  final String id;
+  final String title;
+  final String level;
+  final String category;
+  final bool isPremium;
+
+  const LessonModel({
+    required this.id,
+    required this.title,
+    required this.level,
+    required this.category,
+    this.isPremium = false,
+  });
+}
+
+class AchievementModel {
+  final String id;
+  final String title;
+  final String description;
+  final bool unlocked;
+
+  const AchievementModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.unlocked = false,
+  });
+}
