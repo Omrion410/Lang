0357b79988883c618f55a84c726acb6b4b22947d
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 16:00:05 +0100

    Add authentication and lesson services

--- EnglishGo/mobile/lib/services.dart
+++ EnglishGo/mobile/lib/services.dart
@@ -1,1 +1,33 @@
+class AuthService {
+  Future<bool> login(String email, String password) async {
+    return true;
+  }
 
+  Future<bool> register(String name, String email, String password) async {
+    return true;
+  }
+
+  Future<void> logout() async {}
+}
+
+class LessonService {
+  Future<List<Map<String, dynamic>>> getLessons() async {
+    return [];
+  }
+
+  Future<void> completeLesson(String lessonId) async {}
+}
+
+class SpeakingService {
+  Future<String> analyzePronunciation(String text) async {
+    return 'Analysis unavailable';
+  }
+}
+
+class SubscriptionService {
+  Future<bool> isPremium() async {
+    return false;
+  }
+
+  Future<void> startSubscription(String plan) async {}
+}
