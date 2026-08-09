8705d6b036cbcaca94135367c8591b4b2f871f37
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 15:52:58 +0100

    Initialize EnglishGoApp with basic Flutter setup

--- EnglishGo/mobile/lib/app.dart
+++ EnglishGo/mobile/lib/app.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class EnglishGoApp extends StatelessWidget {
+  const EnglishGoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'EnglishGo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.teal,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('EnglishGo'),
+        ),
+      ),
+    );
+  }
+}
