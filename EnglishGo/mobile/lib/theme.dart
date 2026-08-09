110e84fc92b6314975cc37cf08176fab74fff528
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 15:56:00 +0100

    Implement light and dark themes in theme.dart
    Added light and dark theme configurations using ThemeData.

--- EnglishGo/mobile/lib/theme.dart
+++ EnglishGo/mobile/lib/theme.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.teal,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.teal,
+    brightness: Brightness.dark,
+  );
+}
