8752bc4d33b2f02bd520c363669c6355531d4842
Omrion410 <Omrion.dev@gmail.com>
2026-08-09 15:51:05 +0100

    Add AppConstants class with application constants

--- EnglishGo/mobile/lib/constants.dart
+++ EnglishGo/mobile/lib/constants.dart
@@ -1,1 +1,32 @@
+class AppConstants {
+static const String appName = 'EnglishGo';
+
+static const String appVersion = '1.0.0';
+
+static const String defaultLanguage = 'en';
+
+static const String arabicLanguage = 'ar';
+
+static const String englishLanguage = 'en';
+
+static const String welcomeRoute = '/';
+
+static const String homeRoute = '/home';
+
+static const String loginRoute = '/login';
+
+static const String registerRoute = '/register';
+
+static const String lessonsRoute = '/lessons';
+
+static const String profileRoute = '/profile';
+
+static const String settingsRoute = '/settings';
+
+static const int freeDailyLessons = 5;
+
+static const int maxLearningStreakDays = 365;
+
+static const String supportEmail = '[support@englishgo.app](mailto:support@englishgo.app)';
+}
 
